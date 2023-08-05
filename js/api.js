e9f4f1eca28973677bef676b3af1cee8f30eb3ea
Juliana Lucca <lucca.editora@gmail.com>
2023-08-05 15:36:49 -0300

    fix: content language

--- js/api.js
+++ js/api.js
@@ -2,14 +2,14 @@ import { apiKey } from '../environment/key.js'
 
 // [API Request]
 async function searchMovieByName(title) {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${title}&language=en-US&page=1`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${title}&language=pt-BR&page=1`;
     const fetchResponse = await fetch(url);
     const { results } = await fetchResponse.json();
     return results
 };
 
 async function getPopularMovies() {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`;
     const fetchResponse = await fetch(url);
     const { results } = await fetchResponse.json();
     return results;
